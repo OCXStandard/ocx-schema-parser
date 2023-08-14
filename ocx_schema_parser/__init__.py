bc4e02d4a7b0e45cc3a08f3fefedba8494dbd9ef
ocastrup <ole.christain.astrup@dnv.com>
2023-08-14 08:17:46 +0200

    Bump to 0.7.0

--- ocx_schema_parser/__init__.py
+++ ocx_schema_parser/__init__.py
@@ -6,7 +6,7 @@ from loguru import logger
 #Application imports
 from .utils import utilities
 
-__version__ = '0.6.0'
+__version__ = '0.7.0'
 
 current_dir = Path(__file__).parent
 config_file = current_dir / "configs" / "schema_config.yaml"  # The schema config
