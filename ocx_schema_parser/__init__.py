aecef6ad0a6b606691a7fd402cc0ab2317d3edcc
Ole Christian Astrup <ole.christian.astrup@dnv.com>
2026-09-07 12:40:46 +0200

    Bump to v3.1.0

--- ocx_schema_parser/__init__.py
+++ ocx_schema_parser/__init__.py
@@ -19,7 +19,7 @@ from ocx_schema_parser.model import (
 )
 from ocx_schema_parser.resolver import resolve
 
-__version__ = "3.0.0"
+__version__ = "3.1.0"
 
 DEFAULT_SCHEMA = "https://3docx.org/fileadmin/ocx_schema/V320/OCX_Schema.xsd"
 WORKING_DRAFT = "https://3docx.org/fileadmin//ocx_schema//V320rc8//OCX_Schema.xsd"
